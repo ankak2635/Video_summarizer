06da545dcbebb44f018c3fc340230e74042e0125
Ankit <ank.ak2635@gmail.com>
2024-07-06 21:49:20 +0530

    code fix

--- src/utils.py
+++ src/utils.py
@@ -1,14 +1,16 @@
 import yt_dlp
 import whisper 
 import textwrap
+import torch
+from dotenv import load_dotenv
 
-from langchain import OpenAI
-from  langchain.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain.prompts import PromptTemplate
 from langchain.chains.summarize import load_summarize_chain
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.docstore.document import Document
 
-
+load_dotenv()
 
 class utils():
     def __init__(self):
@@ -67,8 +69,11 @@ class utils():
         """
 
         try:
+            device = "cuda:0" if torch.cuda.is_available() else "cpu"
+
             # Transcribe the audio content using the 'tiny' Whisper model
             model = whisper.load_model('tiny')
+            model.to(device)
             transcription = model.transcribe('./audio.mp3')
 
             # save as text file
@@ -112,62 +117,67 @@ class utils():
             print(f"An error occurred during text splitting: {e}")
 
 
-    # summarize the transcript
-    def summarize_text(self,docs):
+
+    def summarize_text(self, docs):
         """
-        Split and segment a text document into smaller chunks for further processing.
+    Summarizes the provided text documents using OpenAI's GPT-3.5-turbo model.
 
-        This function takes a text document, divides it into smaller segments, and creates a list of document objects.
-        It is typically used in a larger workflow, such as transcribing a video and then segmenting the transcription into manageable portions for summarization.
+    This function takes a list of text documents, constructs a custom prompt to request
+    a concise summary, and processes the request through a summarization chain using the
+    GPT-3.5-turbo model. The resulting summary is then formatted for readability.
 
-        Returns:
-            List[Document]: A list of document objects, each containing a portion of the text document.
+    Args:
+        docs (list of str): A list of text documents to be summarized.
 
-        Raises:
-            Exception: If any errors occur during text splitting, an error message is printed.
+    Returns:
+        str: A formatted string containing the concise summary of the input text documents.
+
+    Raises:
+        Exception: If any unexpected errors occur during the summarization process, 
+                   an error message is printed.
         """
-        
+
         try:
-
-            # load the openai model
-            llm=OpenAI(model='text-davinci-003', temperature=0)
-
-            # define the prompt template
+            # Define the prompt template
             prompt_template = """Write a concise summary of the following:
 
             {text}
 
-            CONSCISE SUMMARY:"""
+            CONCISE SUMMARY:"""
 
             custom_prompt = PromptTemplate(template=prompt_template, input_variables=['text'])
 
+            llm = ChatOpenAI(temperature=0, model='gpt-3.5-turbo')
+
+            # Prepare the chain
             chain = load_summarize_chain(
                 llm=llm,
                 chain_type='stuff',
-                prompt = custom_prompt
+                prompt=custom_prompt
             )
 
-            # get the summary
-            output_summary = chain.run(docs)
+            # Get the summary
+            response = chain.invoke(docs)
+            output_summary = response["output_text"]
 
-            # format text
-            wrapped_text = textwrap.fill(output_summary, 
-                             width=100,
-                             break_long_words=False,
-                             replace_whitespace=False)
-        
+            # Format text
+            wrapped_text = textwrap.fill(output_summary,
+                                        width=100,
+                                        break_long_words=False,
+                                        replace_whitespace=False)
+
             return wrapped_text
-
 
         except Exception as e:
             print(f"An error occurred during summarization: {e}")
 
 
-
 # if __name__ == '__main__':
-#     # url = 'https://www.youtube.com/watch?v=mBjPyte2ZZo'
+#     # url = 'https://www.youtube.com/watch?v=bxuYDT-BWaI'
 #     obj = utils()
-#     res = obj.summarize_text()
-#     print(res)
+#     doc = obj.split_transcript()
+#     summ = obj.summarize_text(doc)
+#     print(summ)
+    
   
     
